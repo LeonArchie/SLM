b6fda338edee9f3a982b03df0d8babb50150d6ba
Archie <levmaikhailovish@ya.ru>
2025-01-19 13:05:08 +0300

    v.144 add index.php

--- web/info.php
+++ web/info.php
@@ -1,3 +1,57 @@
 <?php
-phpinfo();
+	//session_start();
+	// Проверка, авторизован ли пользователь
+	//if (!isset($_SESSION['user']) || !isset($_COOKIE['session_id']) || $_COOKIE['session_id'] !== session_id()) {
+		// Если пользователь не авторизован или куки не совпадают, перенаправляем на страницу авторизации
+		//header("Location: logout.php");
+	//exit();
+	//}
 ?>
+<!DOCTYPE html> 											
+<html lang="ru">
+	<head>
+		<!--Заголовок-->
+		<title>SLM</title>	
+		<!--Кодировка-->
+		<meta charset="utf-8">							
+		 <!-- Адаптивность -->
+		<meta											
+			name="viewport"
+			content="width=device-width, initial-scale=1.0"
+		/>
+		<!--Ключевые слова-->
+		<meta
+			name="description"
+			content="Управление жизненным циклом серверов и приложений"
+		/>
+		<!--Минус роботы-->
+		<meta 
+			name="robots"
+			content="noindex, nofollow" 
+		/>
+		<!-- Подключение стилей -->
+		<link 
+			rel="stylesheet" 
+			href="/css/navbar.css"
+		/>
+		<link 
+			rel="stylesheet" 
+			href="/css/all.css"
+		/>
+		<!-- Фавикон -->
+		<link
+			rel="icon"
+			sizes="16x16 32x32 48x48"
+			type="image/png"
+			href="/img/icon.png"
+		/>
+	</head>
+	<body>
+		<?php include 'header.html'; ?>
+		<?php include 'navbar.html'; ?>
+		<main>
+            <?php phpinfo(); ?>
+		</main>
+		<?php include 'footer.html'; ?>	
+	</body>
+</html>
