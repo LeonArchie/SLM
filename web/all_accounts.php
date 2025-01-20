6e3fd73af8287bee60aec5d358278f5737ad6262
Archie <levmaikhailovish@ya.ru>
2025-01-20 16:45:20 +0300

    v.1.169 add all_head update all_accounts

--- web/all_accounts.php
+++ web/all_accounts.php
@@ -8,42 +8,13 @@
 <!DOCTYPE html> 											
 <html lang="ru">
 	<head>
-		<!--Заголовок-->
-		<title>SLM</title>	
-		<!--Кодировка-->
-		<meta charset="utf-8">							
-		<!--Ключевые слова-->
-		<meta
-			name="description"
-			content="Управление жизненным циклом серверов и приложений"
-		/>
-		<!--Минус роботы-->
-		<meta 
-			name="robots"
-			content="noindex, nofollow" 
-		/>
-		<!-- Подключение стилей -->
-		<link 
-			rel="stylesheet" 
-			href="/css/navbar.css"
-		/>
-		<link 
-			rel="stylesheet" 
-			href="/css/all.css"
-		/>
-		<!-- Фавикон -->
-		<link
-			rel="icon"
-			sizes="16x16 32x32 48x48"
-			type="image/png"
-			href="/img/icon.png"
-		/>
+		<?php include 'include/all_head.html'; ?>	
 	</head>
 	<body>
 		<?php include 'include/header.html'; ?>
 		<?php include 'include/navbar.html'; ?>
 		<main>
-			<h1>Добро пожаловать, <?php echo isset($_SESSION['username']) ? htmlspecialchars($_SESSION['username']) : 'Гость'; ?>!</h1>!</h1>
+			<h1>Добро пожаловать, <?php echo isset($_SESSION['username']) ? htmlspecialchars($_SESSION['username']) : 'Гость'; ?>!</h1>
 			<p>Просмотреть все аккаунты</p>
 			<p> Страница в разработке</p>
 			<p>ID вашей сессии: <?php echo htmlspecialchars($_SESSION['session_id']); ?></p>
